7c15c8139bbc3ebc38a22e2d2cc7df4878962089
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-30 01:53:35 +0900

    ex3-4

---
+++ ex3-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main () {
+	int a, b, c,d;
+	a = 1250;
+	b = -345;
+	c = 32000;
+	d = -12;
+	printf("* 데이터1 * * * 데이터 2 *\n");
+	printf("%12d %14d\n", a, b);
+	printf("%12d %14d\n", c, d);
+}
