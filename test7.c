ad6e162de51dd65b5821206feb70d6a3092f4fbe
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-11 11:02:25 +0900

    test7

---
+++ test7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main() {
+	int x = 200;
+	int y = 13;
+	
+	printf("%d\n", x+y);
+	printf("200 + 13= %d\n", x+y);
+	printf("200 - 13= %d\n", x-y);
+	printf("200 X 13 = %d\n", x*y);
+	printf("200 / 13 = %d ---> ³ª¸ÓÁö= %d\n", x/y, x%y);
+}
