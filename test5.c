5929ca6e19a6e0b326144d791af19266bf934338
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-11 11:03:09 +0900

    test5

---
+++ test5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+	printf("\n");
+	printf("********************\n");
+	printf("hardware\n");
+	printf("software\n");
+	printf("\n\n");
+	printf("monitor printer\n");
+	printf("********************\n");
+	printf("It's 30\%%\n");
+	printf("<Warning! \"%%%%%%\">\n");
+}
