2ab465626ab2261811e5b01680377e8f3b946a8f
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-12 09:57:57 +0900

    ex4-27

---
+++ ex4-27.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+main() {
+	printf("%d bytes\n", sizeof(int));
+	printf("%d bytes\n", sizeof(short));
+	printf("size of long type = %d bytes\n", sizeof(long));
+	printf("size of character type = %d bytes\n", sizeof(char));
+	printf("size of float type = %d bytes\n", sizeof(float));
+	printf("size of double type = %d bytes\n", sizeof(double));
+}
