d57222ea68df6e704862769270ef4db105fb829a
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-16 22:08:28 +0900

    ex4-1

---
+++ ex4-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main () {
+	int a, b;
+	a = 100, b = 50;
+	printf("蟾晦 高, a=%d b=%d\n", a,b);
+	a += 100;
+	printf("渦и 高, a=%d\n", a);
+	b -= 100;
+	printf("賓 高, b=%d\n", b);
+	a *= 30;
+	printf("培и 高, a=%d\n", a);
+}
