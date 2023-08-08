486fc8e2b8481df91492195bec0fd389a300ed2f
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-08 11:01:12 +0900

    ex4-31

---
+++ ex4-31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main() {
+int a, b, c = 30, d = 20;
+float e;
+a = 20.5 + 30.6;
+b = (int)20.5 + (int)30.6;
+e = (float)c/d;
+printf("a = %d, b = %d\n", a, b);
+printf("e = %f\n", e);
+}
+
