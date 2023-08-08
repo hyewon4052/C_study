f5825049ff7fba9bf7ae29be35f9460183caaad8
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-08 11:04:00 +0900

    ex4-12

---
+++ ex4-12.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+main () {
+	int a, b, sum;
+	a = 10, b = 20, sum = 40;
+	printf("a=%d, b = %d\n", a, b);
+	sum += b * ++a;
+	printf("ÃÖÁ¾°ª a = %d, b = %d, sum = %d\n", a, b, sum);
+}
